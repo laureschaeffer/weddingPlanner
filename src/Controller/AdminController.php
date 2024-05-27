9e4c80887bde768d7f847b36777b8416408d07f8
laureschaeffer <laure.schaeffer@hotmail.com>
2024-05-27 16:12:11 +0200

    ajout du traitement des demandes de contact dans le pannel admin

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,9 @@
 //------------------------------------------------------------------------pannel admin---------------------------------------------------------------------
 namespace App\Controller;
 
+use App\Entity\Project;
 use App\Entity\Testimony;
+use App\Repository\ProjectRepository;
 use App\Repository\TestimonyRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,6 +18,59 @@ class AdminController extends AbstractController
     {
         return $this->render('admin/index.html.twig', []);
     }
+
+    //----------------------------------------------partie demandes de contact--------------------------------
+
+    //liste des demandes reçues
+    #[Route('/coiffe/projet', name: 'app_projet')]
+    public function listProject(ProjectRepository $projectRepository): Response
+    {
+        $projetNonTraites = $projectRepository->findBy(['isContacted' => 0], ['dateEvent' => 'ASC']);
+        $projetTraites = $projectRepository->findBy(['isContacted' => 1], ['dateEvent' => 'ASC']);
+
+        return $this->render('admin/listeProject.html.twig', [
+            'projetNonTraites' => $projetNonTraites,
+            'projetTraites' => $projetTraites
+        ]);
+    }
+
+    //detail d'une demande
+    #[Route('/coiffe/projet/{id}', name: 'show_projet')]
+    public function showProject(Project $project = null): Response
+    {
+        //si l'id passé dans l'url existe; possible comme je mets project en null par defaut en argument, sinon erreur
+         if($project){
+            return $this->render('admin/showProject.html.twig', [
+                'project' => $project
+            ]);
+
+        } else {
+            //msg d'erreur
+            return $this->redirectToRoute('app_creation');
+        }
+    }
+
+    //change la demande de contact en traitée ou repasse en à traiter
+    #[Route('/coiffe/changeProjet/{id}', name: 'change_projet')]
+    public function changeProjectState(EntityManagerInterface $entityManager, Project $project = null){
+
+        //si isContacted est vraie, on le passe en faux
+        if($project->isContacted()){
+            $project->setContacted(false);
+        } else {
+            $project->setContacted(true);
+        }
+
+        $entityManager->persist($project);
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Statut de la demande changé');
+        return $this->redirectToRoute('show_projet', ['id'=>$project->getId()]);
+
+    }
+
+
+    //----------------------------------------------partie témoignages--------------------------------
 
     //affiche la page pour verifier les avis
     #[Route('/coiffe/avis', name: 'app_avis')]
