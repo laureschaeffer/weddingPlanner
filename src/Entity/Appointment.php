2670ec2b860336f1f72fced24c02aa1fb74111bc
laureschaeffer <laure.schaeffer@hotmail.com>
2024-07-25 22:06:53 +0200

    ajout d'une entité appointment pour une gestion de prise de rendez-vous

---
+++ src/Entity/Appointment.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AppointmentRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AppointmentRepository::class)]
+class Appointment
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'appointments')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $User = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTime $dateStart = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTime $dateEnd = null;
+
+    public function __construct()
+    {
+        //crée une copie indépendante de dateStart pour ne pas la modifier directement
+        $this->setDateEnd((clone $this->dateStart)->modify('+1 hour'));
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+
+    public function getUser(): ?User
+    {
+        return $this->User;
+    }
+
+    public function setUser(?User $User): static
+    {
+        $this->User = $User;
+
+        return $this;
+    }
+
+    public function getDateStart(): ?\DateTime
+    {
+        return $this->dateStart;
+    }
+
+    public function setDateStart(\DateTime $dateStart): static
+    {
+        $this->dateStart = $dateStart;
+
+        return $this;
+    }
+
+    public function getDateEnd(): ?\DateTime
+    {
+        return $this->dateEnd;
+    }
+
+    public function setDateEnd(?\DateTime $dateEnd): static
+    {
+        $this->dateEnd = $dateEnd;
+
+        return $this;
+    }
+
+    public function __toString()
+    {
+        return $this->dateStart;
+    }
+
+    // -----------------------------------methodes-----------------------------------
+    
+    //verifie si la date de rdv n'est pas dépassée
+    public function isValidDate() : bool 
+    {
+        $ajd = new \DateTime();
+
+        if($this->dateStart < $ajd){
+            return false; 
+        } else {
+            return true ;
+        }
+    }
+
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -58,10 +58,17 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(length: 255)]
     private ?string $google_id = null;
 
+    /**
+     * @var Collection<int, Appointment>
+     */
+    #[ORM\OneToMany(targetEntity: Appointment::class, mappedBy: 'User')]
+    private Collection $appointments;
+
     public function __construct()
     {
         $this->reservations = new ArrayCollection();
         $this->comments = new ArrayCollection();
+        $this->appointments = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -249,6 +256,36 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     public function setGoogleId(string $google_id): static
     {
         $this->google_id = $google_id;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Appointment>
+     */
+    public function getAppointments(): Collection
+    {
+        return $this->appointments;
+    }
+
+    public function addAppointment(Appointment $appointment): static
+    {
+        if (!$this->appointments->contains($appointment)) {
+            $this->appointments->add($appointment);
+            $appointment->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAppointment(Appointment $appointment): static
+    {
+        if ($this->appointments->removeElement($appointment)) {
+            // set the owning side to null (unless already changed)
+            if ($appointment->getUser() === $this) {
+                $appointment->setUser(null);
+            }
+        }
 
         return $this;
     }
