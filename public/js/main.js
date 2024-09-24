0f560bd59f62ad208ea1b3f71ca9051ade518719
laureschaeffer <laure.schaeffer@hotmail.com>
2024-09-24 09:51:48 +0200

    ajout nav profil utilisateur et iframe google maps

--- public/js/main.js
+++ public/js/main.js
@@ -7,3 +7,59 @@ const scrollUp = () =>{
 
 }
 window.addEventListener('scroll', scrollUp)
+
+//----------------------------------page profil: navbar----------------------------------
+
+  //les elements de la nav
+  const reservationBtn = document.querySelector('.open-reservation') 
+  const projetBtn = document.querySelector('.open-projet') 
+  const rdvBtn = document.querySelector('.open-rendezvous') 
+  const paramBtn = document.querySelector('.open-parametre') 
+  
+  //si on trouve cet élément donc que nous sommes sur la page profil
+  if(reservationBtn){
+    //les sections
+    const reservationEl = document.querySelector('#profil-reservation')
+    const projetEl = document.querySelector('#profil-projet')
+    const rdvEl = document.querySelector('#profil-rdv')
+    const paramEl = document.querySelector('#profil-parametre')
+    
+    // par defaut 
+    projetEl.classList.add('active')
+    projetBtn.classList.add('nav-active')
+  
+    //fonction montre la div
+    function showElement(element, navElement){
+      // enleve la classe active à tous les elements et son "btn" de la nav
+      reservationEl.classList.remove('active')
+      projetEl.classList.remove('active')
+      rdvEl.classList.remove('active')
+      paramEl.classList.remove('active')
+  
+      reservationBtn.classList.remove('nav-active')
+      projetBtn.classList.remove('nav-active')
+      rdvBtn.classList.remove('nav-active')
+      paramBtn.classList.remove('nav-active')
+    
+      element.classList.add('active')
+      navElement.classList.add('nav-active')
+    }
+    
+    // ecouteurs d'evenement 
+    reservationBtn.onclick = () => {
+      showElement(reservationEl, reservationBtn)
+    }
+  
+    projetBtn.onclick = () => {
+      showElement(projetEl, projetBtn)
+    }
+  
+    rdvBtn.onclick = () => {
+      showElement(rdvEl, rdvBtn)
+    }
+
+    paramBtn.onclick = () => {
+      showElement(paramEl, paramBtn)
+    }
+
+  }
