510fcc02eed655f82177ef1918342993373672a2
laureschaeffer <laure.schaeffer@hotmail.com>
2024-10-07 16:59:48 +0200

    début fonction qui télécharge un devis

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\BillRepository;
 use App\Repository\ProjectRepository;
+use App\Repository\QuotationRepository;
 use App\Repository\ReservationRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -13,7 +15,7 @@ class SearchController extends AbstractController
 
     #[Route('/search/{word}', name: 'app_search')]
     //renvoie le resultat de la recherche en fonction d'un mot, fonction dans chaque repository ; mettre par défaut word à null pour ne pas avoir d'erreur
-    public function index(ProjectRepository $pr, ReservationRepository $rr, $word = null): Response
+    public function index(ProjectRepository $pr, ReservationRepository $rr, QuotationRepository $qr, $word = null): Response
     {
         //honey pot field
         $honeypot= filter_input(INPUT_POST, "firstname", FILTER_SANITIZE_SPECIAL_CHARS);
@@ -32,7 +34,8 @@ class SearchController extends AbstractController
             return $this->render('admin/research.html.twig', [
                 'word' => $word,
                 'projects' => $pr->findByWord($word),
-                'reservations' => $rr->findByWord($word)
+                'reservations' => $rr->findByWord($word),
+                'bills' => $qr->findFileByName($word)
             ]);
         } else {
             return $this->redirectToRoute('app_admin');
@@ -41,4 +44,29 @@ class SearchController extends AbstractController
             
         
     }
+
+    //télécharge le devis ou la facture associée à la recherche
+    #[Route('/search/file/{fileName}', name: 'download_file')]
+    public function downloadSearchedFile($fileName){
+
+        $fileName = $fileName . ".pdf";
+
+        //si le nom du fichier commence par 'FACT_' alors c'est une facture, sinon un devis
+        $fileType = str_starts_with($fileName, 'FACT_') ? "facture/" : "devis/" ;
+
+        // chemin complet du fichier à partir du nom fourni
+        $filePath = $this->getParameter('kernel.project_dir') . '/public/upload/' . $fileType . $fileName;
+
+
+        // si le fichier n'existe pas
+        if (!file_exists($filePath)) {
+            $this->addFlash('error', 'Ce fichier n\'existe pas');
+            return $this->redirectToRoute('app_admin');
+        }
+
+        // retourne le fichier pour téléchargement
+        return $this->file($filePath, $fileName, 'application/pdf');
+    }
+
+
 }
