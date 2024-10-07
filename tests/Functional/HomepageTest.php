c6844918fb39961f2e753d66ecd8fa0a3c4c0f6a
laureschaeffer <laure.schaeffer@hotmail.com>
2024-10-07 13:34:06 +0200

    ajout commentaires tests

--- tests/Functional/HomepageTest.php
+++ tests/Functional/HomepageTest.php
@@ -1,13 +1,24 @@
 <?php
 
 // test fonctionnel: requete + interagir + tester réponse
-namespace App\Tests\Tests\functional;
+
+/*
+- static::createClient -> crée un client HTTP simulé, permet d'effectuer des requêtes HTTP à l'application sans avoir besoin d'un serveur web réel. Il démarre automatiquement le kernel de Symfony afin que l'application soit prête à recevoir des requêtes.
+- $client->request('GET', '/') -> simule requete http get vers le chemin / (page d'accueil)
+----assertion----
+- $this->assertResponseIsSuccessful() -> verifie que la requete get a abouté une réponse type 200
+- $this->assertEquals(5, count($serviceCards)) -> vérifie que le nb d'éléments avec la classe service-card sur la page est égal à 5
+- $this->assertSelectorTextContains("h1", "...") -> vérifie que le texte du h1 est bien celui-ci
+
+*/
+namespace App\Tests\Functional;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class HomepageTest extends WebTestCase
 {
-    public function testSomething(): void
+    //vérifie 3 comportements de la page d'accueil
+    public function testGetHomePage(): void
     {
         $client = static::createClient(); //boot kernel
         $crawler = $client->request('GET', '/'); //crée une requete
@@ -15,7 +26,7 @@ class HomepageTest extends WebTestCase
         // composants de la page 
         $serviceCards = $crawler->filter('.service-card'); //equivalent du querySelector js
 
-        // -----test assert-----
+        // -----assertions-----
         $this->assertResponseIsSuccessful(); 
         $this->assertEquals(5, count($serviceCards));
         $this->assertSelectorTextContains("h1", "Cérémonie Couture, organisation de mariages d'exception");
--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -1,8 +1,12 @@
 <?php
 
-// self::bootKernel() comme on ne fait pas de requetes http, il faut le démarrer manuellement, pour accéder à des services, via un conteneur
-
-namespace App\Tests\Unit\Entity;
+/*
+ - self::bootKernel() comme on ne fait pas de requetes http, il faut le démarrer manuellement, pour accéder à des services, via un conteneur, pour charger les services de l'application pour les tests
+ $container = static::getContainer(); -> conteneur de services de Symfony est récupéré. Le conteneur contient tous les services de l'application, tels que le validateur, les gestionnaires de bases de données, etc. Ce conteneur est nécessaire pour accéder aux services durant le test.
+- $errors = $container->get('validator')->validate($user); -> utilise le service de validation de Symfony (récupéré via le conteneur de services) pour valider l'objet User. La méthode validate() renvoie un objet ConstraintViolationList, qui contient toutes les violations des règles de validation pour cet objet utilisateur. Dans ce cas, il vérifiera que le champ pseudo de l'utilisateur n'est pas vide et qu'il respecte les contraintes définies dans sa classe. 
+- $this->assertCount(2, $errors); -> Cette ligne est une assertion : elle vérifie que le nombre d'erreurs retournées par le validateur est exactement de 2
+*/
+namespace App\Tests\Unit;
 
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
@@ -33,14 +37,15 @@ class UserTest extends KernelTestCase
         $this->assertCount(0, $errors);
     }
 
-    //nous informe que le pseudo est invalide?
+    //nous informe-t'il bien que le pseudo est invalide?
     public function testInvalidPseudo(){
-        self::bootKernel();
-        $container = static::getContainer();
+        self::bootKernel(); //démarre manuellement le kernel
+        $container = static::getContainer(); //accède à des services
 
-        $user = $this->getEntity();
+        $user = $this->getEntity(); //retourne objet user hydraté
         $user->setPseudo("");
 
+        
         $errors = $container->get('validator')->validate($user);
 
         //on s'attend à 2 erreurs puisque le pseudo ne peut ni etre blanc ni < 2 caractères
