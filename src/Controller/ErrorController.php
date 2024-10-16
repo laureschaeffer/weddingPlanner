851e99cba2d41357acf778a015572d7d90e94653
laureschaeffer <laure.schaeffer@hotmail.com>
2024-10-16 11:45:33 +0200

    controlleur d'erreur 403 et 404, page personnalisée

---
+++ src/Controller/ErrorController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/error', name: 'app_error_')]
+class ErrorController extends AbstractController
+{
+    #[Route('/403', name: '403')]
+    public function error403(): Response
+    {
+        return $this->render('error/403.html.twig');
+    }
+    
+    #[Route('/404', name: '404')]
+    public function error404(): Response
+    {
+        return $this->render('error/404.html.twig');
+    }
+
+}
--- src/Security/Redirect403.php
+++ src/Security/Redirect403.php
@@ -6,8 +6,9 @@ use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\ExceptionEvent;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
-class Redirect404
+class Redirect403
 {
     private $router;
 
@@ -24,11 +25,12 @@ class Redirect404
         // récupère l'exception
         $exception = $event->getThrowable();
         //si c'est un erreur 403
-        if ($exception instanceof NotFoundHttpException) {
+        if ($exception instanceof AccessDeniedHttpException) {
 
             //redirige vers la page d'erreur
-            $response = new RedirectResponse($this->router->generate('app_error_404'));
+            $response = new RedirectResponse($this->router->generate('app_error_403'));
             $event->setResponse($response);
         }
     }
+    
 }
